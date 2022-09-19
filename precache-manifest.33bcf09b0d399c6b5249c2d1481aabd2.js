1bb1523a6c43da36a222807c98577847babfbeda
pengmingtao <pengmingtao2022@163.com>
2022-09-19 21:30:54 +0800

    deploy

---
+++ precache-manifest.33bcf09b0d399c6b5249c2d1481aabd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d30159d1620afc8120f",
+    "url": "/account-loon/css/app.ff228441.css"
+  },
+  {
+    "revision": "281089bc0216107a2d27d84c3ce9e05d",
+    "url": "/account-loon/index.html"
+  },
+  {
+    "revision": "2d30159d1620afc8120f",
+    "url": "/account-loon/js/app.e15a41f0.js"
+  },
+  {
+    "revision": "eae72ad43777eed26556",
+    "url": "/account-loon/js/chunk-vendors.562b3a8d.js"
+  },
+  {
+    "revision": "8cbe3086235e532f7baef9c2b1a24c3b",
+    "url": "/account-loon/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-loon/robots.txt"
+  }
+]);
